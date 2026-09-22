0def8956878689945329811632f2234b00dfebeb
Bijay <68939731+BijayLamichhane@users.noreply.github.com>
2026-09-22 21:58:36 +0545

    feat(notifications): add notification query indexes

--- server/src/models/Notification.js
+++ server/src/models/Notification.js
@@ -32,6 +32,9 @@ const notificationSchema = new mongoose.Schema(
   }
 );
 
+notificationSchema.index({ userId: 1, createdAt: -1 });
+notificationSchema.index({ userId: 1, isRead: 1 });
+
 notificationSchema.virtual("id").get(function () {
   return this._id;
 });
