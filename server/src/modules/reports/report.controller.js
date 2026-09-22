512ad42865136146f8a316b063cc4677cdef99d5
Bijay <68939731+BijayLamichhane@users.noreply.github.com>
2026-09-22 21:36:49 +0545

    feat: add reporter report history controller

--- server/src/modules/reports/report.controller.js
+++ server/src/modules/reports/report.controller.js
@@ -2,6 +2,15 @@ import { reportService } from "./report.service.js";
 import { sendCreated } from "../../utils/response.js";
 
 export class ReportController {
+  async getMine(req, res, next) {
+    try {
+      const reports = await reportService.getMyReports(req.user.id);
+      sendSuccess(res, reports);
+    } catch (error) {
+      next(error);
+    }
+  }
+
   async create(req, res, next) {
     try {
       const report = await reportService.create(req.user.id, req.body);
